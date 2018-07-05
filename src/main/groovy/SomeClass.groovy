e5cd9830ed89a373edc12fc3cc86a7382e88ee65
Bogdan Kryvenko <bogdan.kryvenko1994@gmail.com>
2018-07-05 15:41:28 +0300

    add method groovy

--- src/main/groovy/SomeClass.groovy
+++ src/main/groovy/SomeClass.groovy
@@ -1,5 +1,10 @@
 class SomeClass {
     public static void main(String[] args) {
         println 'groovy out'
+        println adding(1+2)
+    }
+
+    def private adding (i,j){
+        i+j
     }
 }
